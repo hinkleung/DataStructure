052c4f28066ceaf4773491012f6d857a48a70572
chenql <394869042@qq.com>
2020-07-17 10:26:34 +0800

    数组未完成

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import Array.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Array arr = new Array(20);
+        for(int i=0;i<10;i++){
+            arr.addLast(i);
+        }
+
+        arr.addLast(100);
+
+        System.out.println(arr);
+
     }
 }
