e488f32b3f4255ac1be92ceb49696d96bdfeb118
chenql <394869042@qq.com>
2020-07-18 18:30:07 +0800

    2-7 动态数组

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -48,11 +48,11 @@ public class Array<E> {
 
     //向数组中index位置添加元素e
     public void add(int index, E e) {
-        if (size == data.length) {
-            throw new IllegalArgumentException("AddLast failed.Array is full.");
-        }
         if (index < 0 || index > size) {
             throw new IllegalArgumentException("添加失败 index>=0 and index<=size");
+        }
+        if (size == data.length) {
+            resize(2 * data.length);
         }
         for (int i = size - 1; i >= index; i--) {
             data[i + 1] = data[i];
@@ -120,6 +120,11 @@ public class Array<E> {
         }
         size--;
         data[size] = null;
+
+        if(size == data.length/2){
+            resize(data.length/2);
+        }
+
         return ret;
     }
 
@@ -136,6 +141,15 @@ public class Array<E> {
         if (index != -1) {
             remove(index);
         }
+    }
+
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
     }
 
 
