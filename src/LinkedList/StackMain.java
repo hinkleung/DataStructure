065a0ca4db17b9b9eb29586e10ad4d4ac1cb000d
chenql <394869042@qq.com>
2020-07-26 23:09:33 +0800

    4-6 链表实现栈

---
+++ src/LinkedList/StackMain.java
@@ -0,0 +1,37 @@
+package LinkedList;
+
+import Stack.Stack;
+import Stack.ArrayStack;
+
+import java.util.Random;
+
+public class StackMain {
+
+    public static double testStack(Stack<Integer> stack, int opCount) {
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        }
+        for (int i = 0; i < opCount; i++) {
+            stack.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 100000;
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<Integer>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + "s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<Integer>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + "s");
+    }
+
+
+
+}
