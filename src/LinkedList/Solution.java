ec5af3f29799a039c886e8cbd8a90cf9a83ace29
chenql <394869042@qq.com>
2020-07-28 23:11:12 +0800

    5-1 leetcode 203号问题，while循环方法解决

---
+++ src/LinkedList/Solution.java
@@ -0,0 +1,89 @@
+package LinkedList;
+
+/**
+ * 删除链表中值等于给定的val的结点
+ */
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeElements1(ListNode head, int val) {
+        //持续判断头结点
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = prev.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 简化
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements2(ListNode head, int val) {
+        //持续判断头结点
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return head;
+    }
+
+    /**
+     * 虚拟头结点
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements3(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                prev.next = prev.next.next;
+            } else {
+                prev = prev.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+}
