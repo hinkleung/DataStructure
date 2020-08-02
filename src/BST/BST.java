f9c4bfe6807d7b6742104109f9632e58ce414479
chenql <394869042@qq.com>
2020-08-02 21:37:44 +0800

    6-3 二分搜索数添加元素

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -28,6 +28,37 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
-    
+
+    public void add(E e) {
+
+        if (root == null) {
+            root = new Node(e);
+            size++;
+        } else {
+            add(root, e);
+        }
+    }
+
+    // 向以node为根的二分搜索树中插入元素E，递归算法
+    private void add(Node node, E e) {
+
+        if (e.equals(node.e)) {
+            return;
+        } else if (e.compareTo(node.e) < 0) {
+            if (node.left == null) {
+                node.left = new Node(e);
+                size++;
+                return;
+            }
+            add(node.left, e);
+        } else {
+            if (node.right == null) {
+                node.right = new Node(e);
+                size++;
+                return;
+            }
+            add(node.right, e);
+        }
+    }
 
 }
