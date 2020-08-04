c9700db79c83d9534796be4b5e57ecc904dabf46
chenql <394869042@qq.com>
2020-08-04 20:40:48 +0800

    6-7 前中后序遍历递归实现

---
+++ src/BST/Main.java
@@ -0,0 +1,21 @@
+package BST;
+
+public class Main {
+
+    //     5
+    //  3     6
+    //2   4      8
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<Integer>();
+        int[] nums = {5,3,6,8,4,2};
+        for (int num : nums) {
+            bst.add(num);
+        }
+        bst.preOrder();
+//      System.out.println(bst.toString());
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
+    }
+}
