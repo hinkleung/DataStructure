c44bc940131d01685249c67d08f5b4ef977fc241
chenql <394869042@qq.com>
2020-08-15 21:34:51 +0800

    8-2 最大堆的数组表示

---
+++ src/MaxHeap/Array.java
@@ -0,0 +1,163 @@
+package MaxHeap;
+
+public class Array<E> {
+
+    private E[] data;
+    private int size;
+
+    /**
+     * 传入数组的容量capacity构造Array
+     *
+     * @param capacity
+     */
+    public Array(int capacity) {
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    /**
+     * 无参数的构造函数，默认数组的容量capacity=10
+     */
+    public Array() {
+        this(10);
+    }
+
+    //获取数组中的元素个数
+    public int getSize() {
+        return size;
+    }
+
+    //获取数组的容量
+    public int getCapacity() {
+        return data.length;
+    }
+
+    //返回数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    //向所有元素后添加一个新元素
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    //向数组中index位置添加元素e
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("添加失败 index>=0 and index<=size");
+        }
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+        data[index] = e;
+        size++;
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d , capacity = %d\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed.索引范围不对");
+        }
+        return data[index];
+    }
+
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed.索引范围不对");
+        }
+        data[index] = e;
+    }
+
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //不存在则返回-1
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if (data[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed.索引范围不对");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        data[size] = null;
+
+        if (size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+
+        return ret;
+    }
+
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public E getFirst(){
+        return get(0);
+    }
+
+}
