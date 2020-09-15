a6c060cb0944bcb35558548b2b423d3d49895313
chenql <394869042@qq.com>
2020-09-15 22:15:02 +0800

    14-5 实现自己的哈希表

---
+++ src/HashCode/HashTable.java
@@ -0,0 +1,79 @@
+package HashCode;
+
+import java.util.TreeMap;
+
+public class HashTable<K, V> {
+
+    /**
+     * TreeMap底层是红黑树
+     */
+    private TreeMap<K, V>[] hashtable;
+    private int M;
+    private int size;
+
+    public HashTable(int M) {
+        this.M = M;
+        size = 0;
+        hashtable = new TreeMap[M];
+        for (int i = 0; i < M; i++) {
+            hashtable[i] = new TreeMap<>();
+        }
+    }
+
+    public HashTable() {
+        this(97);
+    }
+
+    /**
+     * 真正的哈希函数，获取数组中的索引位置
+     *
+     * @param key
+     * @return
+     */
+    private int hash(K key) {
+        //取绝对值
+        return (key.hashCode() & 0x7fffffff) % M;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void add(K key, V value) {
+        //获取索引的TreeMap
+        TreeMap<K, V> map = hashtable[hash(key)];
+        if (map.containsKey(key)) {
+            map.put(key, value);
+        } else {
+            map.put(key, value);
+            size++;
+        }
+    }
+
+    public V remove(K key) {
+        TreeMap<K, V> map = hashtable[hash(key)];
+        V ret = null;
+        if (map.containsKey(key)) {
+            ret = map.remove(key);
+            size--;
+        }
+        return ret;
+    }
+
+    public void set(K key, V value) {
+        TreeMap<K, V> map = hashtable[hash(key)];
+        if (!map.containsKey(key)) {
+            throw new IllegalArgumentException(key + " doesn't exist!");
+        }
+        map.put(key, value);
+    }
+
+    public boolean contains(K key) {
+        return hashtable[hash(key)].containsKey(key);
+    }
+
+    public V get(K key) {
+        return hashtable[hash(key)].get(key);
+    }
+
+}
